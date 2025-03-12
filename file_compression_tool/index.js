e60f1cc7fef36d4ba17ae753d7aa1ba21d54af02
Alexander Tamayo Pino <alextamayop@gmail.com>
2025-03-12 17:07:23 -0500

    Working reading files

--- file_compression_tool/index.js
+++ file_compression_tool/index.js
@@ -1,2 +1,11 @@
-const filepath = process.cwd();
-console.log(filepath);
+import fs from 'node:fs';
+import path from 'node:path';
+
+const filepath = path.join(process.cwd(), '/files/test.txt');
+fs.readFile(filepath, 'utf-8', (err, data) => {
+    if (err) return console.error(err);
+    console.log(data);
+    return data;
+})
+
+fs.createReadStream(filepath, { start: 0, end: 10 });
